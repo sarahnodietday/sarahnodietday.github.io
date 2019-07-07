defacf1e139bfed47b3b35984b3a99169ff68bd0
Sarahnodietday <contact@sarahnodietday.com>
2019-07-08 00:58:55 +0200

    sdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -229,7 +229,7 @@
 			BMI = bodyWeightInKG / (bodyHeightInMeter * bodyHeightInMeter),
 			roundBMI=Math.round(BMI * 100) / 100;
 		
-		$result.text(roundBMI);
+		$result.text(bodyWeightInKG);
 	});
     
     //Ajax Contact Form Start
